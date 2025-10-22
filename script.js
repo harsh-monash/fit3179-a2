368d0558a49cb0c5a7e21b70241710f60136e57a
Harsh Sharma <harsh.mhs@yahoo.com>
2025-10-22 22:16:01 +1100

    scatter plot added

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
+var vg_1 = "bar-2011-top15-countries.json";
+var vg_2 = "bar-2016-top15-countries.json";
+var vg_3 = "male-female.json";
+var vg_4 = "scatter-plot.json";
 
-var vg_2 = "bar-2011-top15-countries.json";
-var vg_3 = "bar-2016-top15-countries.json";
-var vg_4 = "male-female.json";
-
-vegaEmbed("#bar_chart_2", vg_2).then(function(result) {
+vegaEmbed("#bar_chart_2011", vg_1).then(function(result) {
     
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 
 }).catch(console.error);
 
-vegaEmbed("#bar_chart_3", vg_3).then(function(result) {
+vegaEmbed("#bar_chart_2016", vg_2).then(function(result) {
     
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 
 }).catch(console.error);
 
-vegaEmbed("#male_female_chart", vg_4).then(function(result) {
+vegaEmbed("#male_female_chart", vg_3).then(function(result) {
+
+}).catch(console.error)
+
+vegaEmbed("#age_pop_scatter", vg_4).then(function(result) {
 
 }).catch(console.error)
