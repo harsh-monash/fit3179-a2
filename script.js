e00995bf445340321d50ebe1ea7e596ec21f4c55
Harsh Sharma <harsh.mhs@yahoo.com>
2025-10-22 23:30:58 +1100

    chloromap + discussions

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var vg_1 = "bar-2011-top15-countries.json";
 var vg_2 = "bar-2016-top15-countries.json";
 var vg_3 = "male-female.json";
 var vg_4 = "scatter-plot.json";
+var vg_5 = "map.json";
 
 vegaEmbed("#bar_chart_2011", vg_1).then(function(result) {
     
@@ -22,5 +23,9 @@ vegaEmbed("#male_female_chart", vg_3).then(function(result) {
 }).catch(console.error)
 
 vegaEmbed("#age_pop_scatter", vg_4).then(function(result) {
+
+}).catch(console.error)
+
+vegaEmbed("#map", vg_5).then(function(result) {
 
 }).catch(console.error)
